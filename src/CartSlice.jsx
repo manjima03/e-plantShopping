efb8dbd201756f76fcc2bdf570cf192d2d91e5da
Manjima <manjmak28@gmail.com>
2025-03-07 22:44:34 +0530

    Updated CartItem.jsx

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,12 +7,13 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-      const existingItem= state.items.find(item=>item.name===action.payload.name);
+      const {name,image,cost} = action.payload;
+      const existingItem= state.items.find(item=>item.name===name);
       if (existingItem){
-        existingItem.quantity+=1;
+        existingItem.quantity++;
       }
       else{
-        state.items.push({...action.payload,quantity: 1});
+        state.items.push({name,image,cost,quantity: 1});
       }
     },
     removeItem: (state, action) => {
